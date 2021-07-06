66d8290ce93fbd2e2fe9fc4d331f8159bc83c177
Tom Chestnut <tchestnut85@gmail.com>
2021-07-06 12:40:10 -0400

    Installed React Navigation to make a navigation bar for routing to different pages/screens. Setup the navigation bar with icons for each of them.

--- App.js
+++ App.js
@@ -5,10 +5,46 @@ import {
 } from "@expo-google-fonts/oswald";
 
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
+import { SafeArea } from "./src/components/utils/SafeArea";
+import { Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { theme } from "./src/infrastructure/theme";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "restaurant",
+  Map: "map",
+  Settings: "settings",
+};
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
+const Restaurants = () => <RestaurantsScreen />;
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -25,7 +61,19 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Navigator
+            screenOptions={createScreenOptions}
+            tabBarOptions={{
+              activeTintColor: "teal",
+              inactiveTintColor: "gray",
+            }}
+          >
+            <Screen name="Restaurants" component={Restaurants} />
+            <Screen name="Map" component={Map} />
+            <Screen name="Settings" component={Settings} />
+          </Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>

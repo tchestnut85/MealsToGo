aaf7a09251fcf51d71d756b3500d8f2cb2b93acd
Tom Chestnut <tchestnut85@gmail.com>
2021-07-06 11:02:18 -0400

    Added a Spacer component to add spacing around other components, and a Text component to style text as needed. Also refactored RestaurantCard component to seperate the styling into a seperate file.

---
+++ src/features/restaurants/components/RestaurantCardStyles.js
@@ -0,0 +1,51 @@
+import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+
+export const Icon = styled.Image`
+  height: 15px;
+  width: 15px;
+`;
+
+export const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const RestaurantCardCover = styled(Card.Cover)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const Content = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const SubContent = styled.View`
+  align-content: space-between;
+  justify-content: space-evenly;
+`;
+
+export const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+export const Statuses = styled.View`
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+export const IsOpen = styled.View`
+  align-items: center;
+`;
